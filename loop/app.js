9a90ae8ee5b57c9da13aa91952341b555d9799d5
WebAsif1 <mdasif606392@gmail.com>
2025-10-30 18:38:32 +0600

    for loop, while loop, do-while loop

--- loop/app.js
+++ loop/app.js
@@ -1,6 +1,40 @@
-// For
+//**For
 // 1-100
 
+// if you know the number of iterations in advance then for loop is preferred.
+// for(start; end; update){
+    // code
+// }
 for(let i=1; i<=100; i++){
     console.log(i);
 };
+
+
+//**while
+// Start
+// while(end){
+  // code
+  // update
+  // }
+
+// if your condition is false initially then while loop will not execute even once.
+let i = 1;
+while(i<=100){
+    console.log(i);
+    i++;
+}
+
+
+//**do-while
+// start
+// do{
+    // code
+    // update
+// } while(end);
+
+// if your condition is false initially then also do-while will execute at least once.
+let j = 29;
+do{
+    console.log (j);
+    j++;
+} while(j<=100);
