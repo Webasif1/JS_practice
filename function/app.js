247f070d0ded9991eac8f8af773680c8b08888a6
webasif <mdh606392@gmail.com>
2025-10-31 21:12:43 +0600

    create function and store function in veriable

---
+++ function/app.js
@@ -0,0 +1,16 @@
+//** Create a function
+function f1() {
+  console.log("Learning function");
+}
+
+//**For run the function write the function name and a first bracket
+f1();
+
+
+//** Store function inside variable
+let fnc = function(){   //can't give name cause variable name is the function name
+  console.log("Variable function");
+}
+
+// To call the function write the variable name
+fnc();
